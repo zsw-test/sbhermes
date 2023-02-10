54e967683ff8d0a68a6f6b5ea92b89489bdb3b40
zhangsiwei <zhangsiwei@kingsoft.com>
2023-02-10 18:18:35 +0800

    docker compose

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.7
+WORKDIR /code
+
+ENV FLASK_APP app.py
+ENV FLASK_RUN_HOST 0.0.0.0
+ENV FLASK_RUN_PORT 5000
+
+
+# # install chromedriver
+# RUN wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip &&\
+# 	unzip chromedriver_linux64.zip chromedriver -d /usr/local/bin/
+
+# # install chrome
+# RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# RUN apt-get install ./google-chrome-stable_current_amd64.deb
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+COPY . .
+CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 app = Flask(__name__)
 
@@ -13,23 +14,37 @@ def sipderhermes():
 	# 实例化对象
 	option = ChromeOptions()
 	# 无头模式
-	option.add_argument("--headless")
+	#option.add_argument("--headless")
 
 	option.add_experimental_option('excludeSwitches',['enable-automation'])# 开启实验性功能
 	# 不关闭窗口
 	option.add_experimental_option("detach", True)
 	# 去除特征值
-	# option.add_argument("--disable-blink-features=AutomationControlled")
-	# 实例化谷歌
-	driver = webdriver.Chrome(options=option)
+	option.add_argument("--disable-blink-features=AutomationControlled")
 
-	driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-	"source": """
+	# 实例化远程谷歌
+	driver = webdriver.Remote(
+		command_executor="http://chrome:4444/wd/hub",
+		desired_capabilities=DesiredCapabilities.CHROME,
+		options=option
+	) 
+
+	# 实例化谷歌
+	# driver = webdriver.Chrome(options=option)
+	driver.execute_script(
+	script="""
 		Object.defineProperty(navigator, 'webdriver', {
 		get: () => undefined
 		})
 	"""
-	})
+	)
+	# driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+	# "source": """
+	# 	Object.defineProperty(navigator, 'webdriver', {
+	# 	get: () => undefined
+	# 	})
+	# """
+	# })
 
 	time.sleep(3)
 	driver.get("https://www.hermes.cn/cn/zh/")
@@ -41,14 +56,10 @@ def sipderhermes():
 		res.append(goodinfo.get_attribute('outerHTML'))
 		print(goodinfo.get_attribute('outerHTML'))
 
-	
+	driver.close()
 	return res
 
 
 
 def hello():
     return "Hello, World!"
-# t3=driver.find_element(By.XPATH,"//*[@id='content']/section[3]/ul/li[1]/div/a/picture/img")
-# print(t.get_attribute('outerHTML'))
-
-# driver.quit()
