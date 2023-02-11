9f8e17d28db7b021837ff49c0a9c1ecabd69c88d
zsw-test <zhang453787298@qq.com>
2023-02-11 13:32:01 +0800

    配置本地环境

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,16 @@ ENV FLASK_APP app.py
 ENV FLASK_RUN_HOST 0.0.0.0
 ENV FLASK_RUN_PORT 5000
 
+# update
+RUN apt-get update -y
+# install chromedriver
+RUN wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip &&\
+	unzip chromedriver_linux64.zip chromedriver -d /usr/local/bin/
 
-# # install chromedriver
-# RUN wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip &&\
-# 	unzip chromedriver_linux64.zip chromedriver -d /usr/local/bin/
+# install chrome
+RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt-get install ./google-chrome-stable_current_amd64.deb -y
 
-# # install chrome
-# RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# RUN apt-get install ./google-chrome-stable_current_amd64.deb
 RUN python -m pip install --upgrade pip -i https://mirrors.bfsu.edu.cn/pypi/web/simple/
 
 COPY requirements.txt requirements.txt
--- app.py
+++ app.py
@@ -8,13 +8,52 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 app = Flask(__name__)
 
+# 本地环境chrome和driver都在一个环境调试
+@app.route("/local")
+def localDev():
+# 实例化对象
+	option = ChromeOptions()
+	# 无头模式 linux环境下必须开无头模式，不然启动不了
+	option.add_argument("--headless")
+	# root环境下跑
+	option.add_argument("--no-sandbox")
+	option.add_experimental_option('excludeSwitches',['enable-automation'])# 开启实验性功能
+	# 不关闭窗口
+	option.add_experimental_option("detach", True)
+	# 去除特征值
+	option.add_argument("--disable-blink-features=AutomationControlled")
 
-@app.route("/")
+
+	# 实例化谷歌
+	driver = webdriver.Chrome(options=option)
+	driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+	"source": """
+		Object.defineProperty(navigator, 'webdriver', {
+		get: () => undefined
+		})
+	"""
+	})
+
+	time.sleep(3)
+	driver.get("https://www.hermes.cn/cn/zh/")
+	goods=driver.find_elements(By.XPATH,"//*[@id='content']/section[3]/ul/li")
+
+	res =[]
+	for good in goods:
+		goodinfo = good.find_element(By.TAG_NAME,"img")
+		res.append(goodinfo.get_attribute('outerHTML'))
+		print(goodinfo.get_attribute('outerHTML'))
+
+	#driver.close()
+	return res
+
+
+@app.route("/remote")
 def sipderhermes():
 	# 实例化对象
 	option = ChromeOptions()
 	# 无头模式
-	#option.add_argument("--headless")
+	option.add_argument("--headless")
 
 	option.add_experimental_option('excludeSwitches',['enable-automation'])# 开启实验性功能
 	# 不关闭窗口
@@ -29,8 +68,6 @@ def sipderhermes():
 		options=option
 	) 
 
-	# 实例化谷歌
-	# driver = webdriver.Chrome(options=option)
 	driver.execute_script(
 	script="""
 		Object.defineProperty(navigator, 'webdriver', {
@@ -38,13 +75,6 @@ def sipderhermes():
 		})
 	"""
 	)
-	# driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-	# "source": """
-	# 	Object.defineProperty(navigator, 'webdriver', {
-	# 	get: () => undefined
-	# 	})
-	# """
-	# })
 
 	time.sleep(3)
 	driver.get("https://www.hermes.cn/cn/zh/")
@@ -55,7 +85,8 @@ def sipderhermes():
 		goodinfo = good.find_element(By.TAG_NAME,"img")
 		res.append(goodinfo.get_attribute('outerHTML'))
 		print(goodinfo.get_attribute('outerHTML'))
-
+		
+	time.sleep(3)
 	driver.close()
 	return res
 
